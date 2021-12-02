09b15b00216cc489870381e30cebcca2624084b2
charlesjamb <charles.jambon@gmail.com>
2021-12-02 14:33:13 +0100

    Add users table with name and email filters

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,102 @@
+import React from "react";
 import Head from "next/head";
 import useSWR from "swr";
+import { useTable, useFilters } from "react-table";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
 
 import { fetcher } from "../utils.js";
+
+function TextFilter({ column: { filterValue, setFilter } }) {
+  return (
+    <TextField
+      variant="standard"
+      value={filterValue || ""}
+      onChange={(e) => {
+        setFilter(e.target.value || undefined);
+      }}
+      placeholder="filtre"
+    />
+  );
+}
+
+function UsersTable({ columns, data }) {
+  const defaultColumn = React.useMemo(
+    () => ({
+      Filter: TextFilter,
+    }),
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable(
+      {
+        columns,
+        data,
+        defaultColumn,
+      },
+      useFilters
+    );
+
+  return (
+    <TableContainer component={Card}>
+      <Table {...getTableProps()}>
+        <TableHead>
+          {headerGroups.map((headerGroup, i) => (
+            <TableRow key={i} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, j) => (
+                <TableCell key={j} {...column.getHeaderProps()}>
+                  <span className="font-bold">{column.render("Header")}</span>
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableHead>
+        <TableBody {...getTableBodyProps()}>
+          {rows.map((row, i) => {
+            prepareRow(row);
+            return (
+              <TableRow key={i} {...row.getRowProps()}>
+                {row.cells.map((cell, j) => (
+                  <TableCell key={j} {...cell.getCellProps()}>
+                    {cell.render("Cell")}
+                  </TableCell>
+                ))}
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 
 export default function Home() {
   const { data, error } = useSWR(
     "https://jsonplaceholder.typicode.com/users",
     fetcher
+  );
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Email",
+        accessor: "email",
+      },
+    ],
+    []
   );
 
   if (error) return <div>failed to load</div>;
@@ -20,12 +110,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="container bg-white m-4">
-        <h1 className="text-red-500 text-xl">Hello World</h1>
-        <p>{data[0].name}</p>
+      <main className="container m-4 w-full">
+        <h1 className="text-3xl font-bold mb-4">Test technique Iconosquare</h1>
+        <UsersTable columns={columns} data={data} />
       </main>
-
-      <footer></footer>
     </div>
   );
 }
